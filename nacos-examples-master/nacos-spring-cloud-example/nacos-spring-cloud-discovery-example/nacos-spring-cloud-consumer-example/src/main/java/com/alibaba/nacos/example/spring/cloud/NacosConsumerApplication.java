c3c1808eb304dd0289bfe1c03a917e4c780bf870
赵鹤 <zhaohe@bonc.com.cn>
2020-05-14 17:27:18 +0800

    回声测试

--- nacos-examples-master/nacos-spring-cloud-example/nacos-spring-cloud-discovery-example/nacos-spring-cloud-consumer-example/src/main/java/com/alibaba/nacos/example/spring/cloud/NacosConsumerApplication.java
+++ nacos-examples-master/nacos-spring-cloud-example/nacos-spring-cloud-discovery-example/nacos-spring-cloud-consumer-example/src/main/java/com/alibaba/nacos/example/spring/cloud/NacosConsumerApplication.java
@@ -1,10 +1,17 @@
 package com.alibaba.nacos.example.spring.cloud;
 
+import org.apache.dubbo.common.URL;
+import org.apache.dubbo.common.extension.ExtensionLoader;
+import org.apache.dubbo.registry.Registry;
+import org.apache.dubbo.registry.RegistryFactory;
+import org.apache.dubbo.rpc.RpcContext;
+import org.apache.dubbo.rpc.service.EchoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ImportResource;
@@ -34,7 +41,17 @@ public class NacosConsumerApplication {
 	}
 
 	public static void main(String[] args) throws InterruptedException {
-		SpringApplication.run(NacosConsumerApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(NacosConsumerApplication.class, args);
+		HelloService helloService = (HelloService) context.getBean("helloService");
+		Person ret = helloService.hello(2);
+		RpcContext rpc = RpcContext.getContext();
+
+		RegistryFactory registryFactory = ExtensionLoader.getExtensionLoader(RegistryFactory.class)
+				.getAdaptiveExtension();
+		Registry registry = registryFactory.getRegistry(URL.valueOf("zookeeper://10.20.153.10:2181"));
+		registry.register(URL.valueOf(
+				"override://0.0.0.0/com.foo.BarService?category=configurators&dynamic=false&application=foo&mock=force:return+null"));
+
 	}
 
 	@RestController
@@ -62,26 +79,32 @@ public class NacosConsumerApplication {
 		@Autowired
 		NotifyImpl demoCallback;
 
-		@RequestMapping(value = "/zookeeper", method = RequestMethod.GET)
-		public String zookeeper() throws InterruptedException {
-			int requestId = 2;
-			Person ret = helloService.hello(requestId);
-			System.out.println(ret == null);
-			System.out.println(demoCallback.ret.containsKey(requestId));
-			for (int i = 0; i < 10; i++) {
-				if (!demoCallback.ret.containsKey(requestId)) {
-					Thread.sleep(200);
-					System.out.println("-------------------------");
-				} else {
-					break;
-				}
-			}
-			System.out.println(demoCallback.ret.containsKey(requestId));
-			return helloService.hello(2).getName();
+		/**
+		 * 静态服务：人为编码注册或者反注册服务，消费者正常使用即可。
+		 */
+
+		/**
+		 * 回声测试
+		 * 
+		 * @return
+		 * @throws InterruptedException
+		 */
+		@RequestMapping(value = "/echo", method = RequestMethod.GET)
+		public boolean echo() throws InterruptedException {
+			EchoService echoService = (EchoService) helloService; // 强制转型为EchoService
+			// 回声测试可用性
+			String status = (String) echoService.$echo("OK");
+			return "OK".equalsIgnoreCase(status);
 		}
 
-		@RequestMapping(value = "/nacos", method = RequestMethod.GET)
-		public String nacos() throws InterruptedException {
+		/**
+		 * 测试事件通知
+		 * 
+		 * @return
+		 * @throws InterruptedException
+		 */
+		@RequestMapping(value = "/eventNotify", method = RequestMethod.GET)
+		public String eventNotify() throws InterruptedException {
 			int requestId = 2;
 			Person ret = helloService.hello(requestId);
 			System.out.println(ret == null);

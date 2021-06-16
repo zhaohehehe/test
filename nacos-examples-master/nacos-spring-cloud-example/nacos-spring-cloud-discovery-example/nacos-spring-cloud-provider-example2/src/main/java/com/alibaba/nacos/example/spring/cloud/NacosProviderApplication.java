9a154ead630b1d1b90682baa4e118be0b654b257
赵鹤 <zhaohe@bonc.com.cn>
2021-06-16 11:55:16 +0800

    增加一个provider测试

---
+++ nacos-examples-master/nacos-spring-cloud-example/nacos-spring-cloud-discovery-example/nacos-spring-cloud-provider-example2/src/main/java/com/alibaba/nacos/example/spring/cloud/NacosProviderApplication.java
@@ -0,0 +1,44 @@
+package com.alibaba.nacos.example.spring.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author xiaojing
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan(basePackages = {"com.alibaba.nacos.example","zhaohe"})
+@ImportResource("classpath:dubbo-provider.xml")
+public class NacosProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NacosProviderApplication.class, args);
+	}
+
+	@RestController
+	class EchoController {
+		@RequestMapping(value = "/echo/{string}", method = RequestMethod.GET)
+		public String echo(@PathVariable String string) {
+			return "Hello Nacos Discovery " + string;
+		}
+	}
+	
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+	   // Do any additional configuration here
+	   return builder.build();
+	}
+	
+	
+}
